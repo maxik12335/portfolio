10d9eb56dda2863aee88eef488f1efc7221d73ef
Max <maxim.it@bk.ru>
2022-06-01 03:29:07 +0300

    add division lists

--- scripts/technologies.js
+++ scripts/technologies.js
@@ -6,3 +6,20 @@ technologiesButtons.forEach((buttons, buttonsIdx) => {
         technologiesList[buttonsIdx].classList.toggle('portfolio__technologies-active')
     })
 })
+
+const buttonAll = document.querySelector('.navigations-all'),
+    buttonReact = document.querySelector('.navigations-react'),
+    listAll = document.querySelector('.portfolio-all'),
+    listReact = document.querySelector('.portfolio-react')
+
+listAll.classList.remove('portfolio-all')
+
+buttonAll.addEventListener('click', () => {
+    listReact.classList.add('portfolio-react')
+    listAll.classList.remove('portfolio-all')
+})
+
+buttonReact.addEventListener('click', () => {
+    listReact.classList.remove('portfolio-react')
+    listAll.classList.add('portfolio-all')
+})
